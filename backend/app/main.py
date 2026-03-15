cbcb1dd4d7dedb59e838e65789f5586b47fea719
millyjain09 <millyjain88@gmail.com>
2026-03-16 01:03:21 +0530

    first commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,8 @@ app = FastAPI(title="AI Interviewer Pro")
 
 origins = [
     "http://localhost:5173", 
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://ai-mockmate-flax.vercel.app"
 ]
 
 app.add_middleware(
